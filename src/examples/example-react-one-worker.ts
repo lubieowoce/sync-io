2a1052c1b9959fbbefa434dd7e671bfb4cbaba42
Janka Uryga <lolzatu2@gmail.com>
2025-02-20 20:11:30 +0100

    add single worker example, reorganize some more

---
+++ src/examples/example-react-one-worker.ts
@@ -0,0 +1,31 @@
+import { Worker } from "node:worker_threads";
+import { createChannel, createClientHandle } from "../lib.js";
+import { runCacheServer } from "./workers/cache-functions.js";
+import type { ReactWorkerData } from "./workers/react-worker.js";
+
+const waitForEnd = async (worker: Worker) => {
+  await new Promise((resolve) => worker.on("exit", () => resolve(undefined)));
+};
+
+(async () => {
+  console.log("root :: hello");
+
+  const channel = createChannel(1);
+  runCacheServer(channel.serverHandle);
+
+  const clientHandle = await createClientHandle(channel);
+
+  const reactWorker = new Worker(
+    new URL(import.meta.resolve("./workers/react-worker.js")),
+    {
+      workerData: { clientHandle, id: 1 } satisfies ReactWorkerData,
+      transferList: [...clientHandle.transferList],
+    }
+  );
+  reactWorker.on("error", (err) => {
+    console.error("Unhandled error in react worker:", err);
+    process.exit(1);
+  });
+
+  await waitForEnd(reactWorker);
+})();
